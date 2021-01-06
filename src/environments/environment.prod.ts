960979d12995077bc7c2db2cbb638f626455ce52
tsognong <tsognong.fidele@gmail.com>
2021-01-06 17:09:58 +0100

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
     production: true,
+    API_HOST: 'https://evening-brushlands-20741.herokuapp.com/horbac-api',
+    keycloakConfig: {
+        clientId: 'horbac-client',
+        realm: 'demo',
+        url: 'http://localhost:8080/auth'
+    }
 };
