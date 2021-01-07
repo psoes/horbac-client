588429f399ffeead32521250781313954ddc21a8
tsognong <tsognong.fidele@gmail.com>
2021-01-07 10:38:19 +0100

    few changes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
-});
-
+app.use(express.static(__dirname + '/dist/horbac'));
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/horbac/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
