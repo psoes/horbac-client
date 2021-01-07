ae492b5a0ef0cc97788f26b3ed23835a2255e44f
tsognong <tsognong.fidele@gmail.com>
2021-01-07 10:10:58 +0100

    few changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.use(static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
