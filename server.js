4c08b25e3dcef41da6ac0f1634a4da5742ee628c
tsognong <tsognong.fidele@gmail.com>
2021-01-07 10:58:18 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/horbac'));
 app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/index.html');
+  const fullPath = path.join(__dirname + '/dist/horbac/index.html');
   console.log(" Fetching from.." + fullPath);
   res.sendFile(fullPath);
 })
