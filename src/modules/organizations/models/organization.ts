2656b4526852d61898e2a762546a09ac8828efed
tsognong <tsognong.fidele@gmail.com>
2021-01-19 16:36:00 +0100

    menu for units and employees added

--- src/modules/organizations/models/organization.ts
+++ src/modules/organizations/models/organization.ts
@@ -1,6 +1,5 @@
 import { Adress } from "@modules/employees/models/Employee";
 import { HOLocation } from "@modules/unities/models/OrgUnit";
-import { Address } from "cluster";
 import { OrganizationType } from "./organization-type";
 
 export class SocialReason {
@@ -16,13 +15,13 @@ export class Organization {
 	cessationDate? : Date;    
     type?: OrganizationType;
     logo?: string;
-    address?: Address;
+    address?: Adress;
     url?: string;
     location?: HOLocation;
     socialReason?: SocialReason;
     constructor(id?: number, name?: string, description?: string, 
         inceptionDate?: Date, cessationDate?: Date, type?: OrganizationType, 
-        address?: Address, url?: string, location?: HOLocation, socialReason?: SocialReason, logo?: string){
+        address?: Adress, url?: string, location?: HOLocation, socialReason?: SocialReason, logo?: string){
         this.id = id;
         this.name = name;
         this.description = description;
